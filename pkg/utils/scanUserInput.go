303c211a74c87a1217d49c489b35ef5337f75dfc
jlouis <louis.jeannot@ynov.com>
2025-10-06 16:53:13 +0200

    fix(utils): little fix on the return of the function scanUserInput

--- pkg/utils/scanUserInput.go
+++ pkg/utils/scanUserInput.go
@@ -1,1 +1,14 @@
 package utils
+
+import "fmt"
+
+func ScanUserInput() (int, int) {
+	a, b := 0, 0
+	PrintPolish()
+	fmt.Println("Enter your first number for the row: (1-9)")
+	fmt.Scan(&a)
+	fmt.Println("Enter your second number for the column: (1-9)")
+	fmt.Scan(&b)
+	PrintPolish()
+	return a - 1, b - 1
+}
